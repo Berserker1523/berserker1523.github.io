521395b12947911b1d18a28fb86ae55af7344a9b
Berserker1523 <berserker1523@hotmail.com>
2019-08-19 11:35:11 -0500

    Arreglado el js de la segunda página

--- js/path.js
+++ js/path.js
@@ -6,8 +6,7 @@ let i;
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
-
-    var content = this.nextElementSibling;
+    let content = this.nextElementSibling;
     if (content.style.display === "block") {
       content.style.display = "none";
     } else {
