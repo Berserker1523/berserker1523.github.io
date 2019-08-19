4348a476208afc218342ebc5fb54d958c12cd7b3
Berserker1523 <berserker1523@hotmail.com>
2019-08-19 10:44:21 -0500

    Arreglados algunos detalles

--- js/path.js
+++ js/path.js
@@ -7,8 +7,6 @@ for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
 
-    $("#asd").dropdown("toggle");
-
     var content = this.nextElementSibling;
     if (content.style.display === "block") {
       content.style.display = "none";
